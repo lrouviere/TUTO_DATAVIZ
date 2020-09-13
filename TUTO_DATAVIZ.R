d4bac37f417ec48f398644aad622a7439c11e50f
Laurent Rouviere <rouviere.laurent@gmail.com>
2020-09-13 15:24:08 +0200

    maj

--- TUTO_DATAVIZ.R
+++ TUTO_DATAVIZ.R
@@ -4,3 +4,178 @@ knitr::write_bib(c(
   .packages(), 'bookdown', 'knitr', 'rmarkdown'
 ), 'packages.bib')
 
+## ---- eval=FALSE, echo=TRUE-------------------------------
+#  demo(graphics)
+
+## ---------------------------------------------------------
+x <- seq(-2*pi,2*pi,by=0.05)
+y <- sin(x)
+plot(x,y) #points (par défaut)
+plot(x,y,type="l") #représentation sous forme de ligne
+
+## ---------------------------------------------------------
+ozone <- read.table("data/ozone.txt")
+summary(ozone)
+
+## ---------------------------------------------------------
+plot(ozone[,"T12"],ozone[,"maxO3"])
+
+## ---------------------------------------------------------
+plot(maxO3~T12,data=ozone)
+
+## ---------------------------------------------------------
+plot(ozone[,"T12"],ozone[,"maxO3"],xlab="T12",ylab="maxO3")
+
+## ---------------------------------------------------------
+hist(ozone$maxO3,main="Histogram")
+barplot(table(ozone$vent)/nrow(ozone),col="blue")
+boxplot(maxO3~vent,data=ozone)
+
+## ---------------------------------------------------------
+library(rAmCharts)
+amHist(ozone$maxO3)
+amPlot(ozone,col=c("T9","T12"))
+amBoxplot(maxO3~vent,data=ozone)
+
+## ----eval=correct,echo=TRUE-------------------------------
+#  mycolors <- c("yellow", "magenta", "orange", "cyan",
+#            "grey", "red", "green", "blue")
+
+## ---------------------------------------------------------
+library(tidyverse)
+set.seed(1234)
+diamonds2 <- diamonds[sample(nrow(diamonds),5000),] 
+summary(diamonds2)
+help(diamonds)
+
+## ---------------------------------------------------------
+plot(price~carat,data=diamonds2)
+
+## ---------------------------------------------------------
+ggplot(diamonds2) #nothing
+ggplot(diamonds2)+aes(x=carat,y=price) #nothing
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_point() #OK
+
+## ----eval=FALSE-------------------------------------------
+#  ggplot(diamonds2)+aes(x=carat,y=price)
+
+## ----eval=FALSE-------------------------------------------
+#  ggplot(diamonds2)+aes(x=carat,y=price,color=cut)
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price,color=cut)+geom_point()
+
+## ---------------------------------------------------------
+D <- data.frame(X=seq(-2*pi,2*pi,by=0.01))
+ggplot(D)+aes(x=X,y=sin(X))+geom_line()
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=price)+geom_histogram(bins=40)
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=price,y=..density..)+geom_histogram(bins=40)
+
+## ----eval=FALSE,echo=TRUE---------------------------------
+#  ggplot(diamonds2)+aes(x=price,y=..density..)+stat_bin()
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price,color=cut)+geom_point()+
+  scale_color_manual(values=c("Fair"="black","Good"="yellow",
+                              "Very Good"="blue","Premium"="red","Ideal"="green"))
+
+## ---------------------------------------------------------
+p1 <- ggplot(diamonds2)+aes(x=cut)+geom_bar(aes(fill=cut))
+p1
+
+## ---------------------------------------------------------
+p1+scale_fill_brewer(palette="Purples")
+
+## ---------------------------------------------------------
+p2 <- ggplot(diamonds2)+aes(x=carat,y=price)+geom_point(aes(color=depth))
+p2
+
+## ---------------------------------------------------------
+p2+scale_color_gradient(low="red",high="yellow")
+
+## ---------------------------------------------------------
+p2+scale_x_continuous(breaks=seq(0.5,3,by=0.5))+scale_y_continuous(name="prix")+scale_color_gradient("Profondeur")
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price,group=cut)+
+  geom_smooth(method="loess")
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price)+
+  geom_smooth(method="loess")+facet_wrap(~cut)
+ggplot(diamonds2)+aes(x=carat,y=price)+
+  geom_smooth(method="loess")+facet_wrap(~cut,nrow=1)
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_point()+
+  geom_smooth(method="lm")+facet_grid(color~cut)
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_point()+
+  geom_smooth(method="lm")+facet_wrap(color~cut)
+
+## ----echo=TRUE,eval=FALSE---------------------------------
+#  ggplot()+aes()+geom_()+scale_()
+
+## ---------------------------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_point()
+ggplot(diamonds2,aes(x=carat,y=price))+geom_point()
+ggplot(diamonds2)+geom_point(aes(x=carat,y=price))
+
+## ---------------------------------------------------------
+X <- seq(-2*pi,2*pi,by=0.001)
+Y1 <- cos(X)
+Y2 <- sin(X)
+donnees1 <- data.frame(X,Y1)
+donnees2 <- data.frame(X,Y2)
+ggplot(donnees1)+geom_line(aes(x=X,y=Y1))+
+  geom_line(data=donnees2,aes(x=X,y=Y2),color="red")
+
+## ---------------------------------------------------------
+p <- ggplot(diamonds2)+aes(x=carat,y=price,color=cut)+geom_point()
+p+theme_bw()
+p+theme_classic()
+p+theme_grey()
+p+theme_bw()
+
+## ---------------------------------------------------------
+data(mtcars)
+summary(mtcars)
+
+## ----echo=FALSE,eval=TRUE---------------------------------
+ggplot(data=diamonds) + geom_boxplot(aes(x=cut,y=carat,fill=cut)) 
+ggplot(data=diamonds) + geom_boxplot(aes(x=cut,y=carat,fill=cut))+coord_flip()
+ggplot(data=diamonds) + geom_density(aes(x=carat,y=..density..)) +  facet_grid(cut~.)
+
+## ----echo=correct,eval=FALSE------------------------------
+#  ggplot(data=diamonds) + geom_boxplot(aes(x=cut,y=carat,fill=cut))
+#  ggplot(data=diamonds) + geom_boxplot(aes(x=cut,y=carat,fill=cut))+coord_flip()
+#  ggplot(data=diamonds) + geom_density(aes(x=carat,y=..density..)) +  facet_grid(cut~.)
+
+## ----echo=correct,eval=TRUE-------------------------------
+Q1 <- diamonds %>% group_by(cut) %>% 
+  summarize(q1=quantile(carat,c(0.25)),q2=quantile(carat,c(0.5)),
+        q3=quantile(carat,c(0.75)))
+quantildf <- Q1%>% gather(key="alpha",value="quantiles",-cut)
+ggplot(data=diamonds) + geom_density(aes(x=carat,y=..density..))+
+  facet_grid(cut~.) +
+  geom_vline(data=quantildf,aes(xintercept=quantiles),col=alpha("black",1/2))
+
+## ----echo=FALSE,eval=TRUE---------------------------------
+library(ggstance)
+ggplot(data=diamonds) +
+  geom_boxploth(data=diamonds,aes(y=-0.5,x=carat,fill=cut)) +
+  geom_density(aes(x=carat,y=..density..)) +  facet_grid(cut~.) +
+  geom_vline(data=quantildf,aes(xintercept=quantiles),col=alpha("black",1/2))+
+  ylab("")
+
+## ----echo=correct,eval=FALSE------------------------------
+#  library(ggstance)
+#  ggplot(data=diamonds) +
+#    geom_boxploth(data=diamonds,aes(y=-0.5,x=carat,fill=cut)) +
+#    geom_density(aes(x=carat,y=..density..)) +  facet_grid(cut~.) +
+#    geom_vline(data=quantildf,aes(xintercept=quantiles),col=alpha("black",1/2))+
+#    ylab("")
+
