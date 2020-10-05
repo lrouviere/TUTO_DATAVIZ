15267dec9c27ff54c572a0a1c727b6bc4e4bf31d
Laurent Rouviere <rouviere.laurent@gmail.com>
2020-10-05 11:37:57 +0200

    maj

--- TUTO_DATAVIZ.R
+++ TUTO_DATAVIZ.R
@@ -37,9 +37,46 @@ amHist(ozone$maxO3)
 amPlot(ozone,col=c("T9","T12"))
 amBoxplot(maxO3~vent,data=ozone)
 
+## ----test-a,teacher=correct-------------------------------
+x <- seq(0,2*pi,length=1000)
+plot(x,sin(x),type="l")
+title("Plot of the sine function")
+
+## ----teacher=correct--------------------------------------
+x <- seq(-4,4,by=0.01)
+plot(x,dnorm(x),type="l")
+abline(v=0,lty=2)
+lines(x,dt(x,5),col=2)
+lines(x,dt(x,30),col=3)
+legend("topleft",legend=c("Gaussian","Student(5)","Student(30)"),
+   col=1:3,lty=1)
+
+## ----teacher=correct--------------------------------------
+spots <- read.table("data/taches_solaires.csv",sep=";",header=TRUE,dec=",")
+
+## ----teacher=correct--------------------------------------
+library(tidyverse)
+period <- cut_interval(spots$annee,n=8)
+
 ## ----eval=correct,echo=TRUE-------------------------------
-#  mycolors <- c("yellow", "magenta", "orange", "cyan",
-#            "grey", "red", "green", "blue")
+mycolors <- c("yellow", "magenta", "orange", "cyan",
+          "grey", "red", "green", "blue")
+
+## ----teacher=correct--------------------------------------
+levels(period) <- mycolors
+
+## ----teacher=correct--------------------------------------
+coordx <- seq(along=spots[,1])
+
+## ----teacher=correct--------------------------------------
+plot(coordx,spots[,1],xlab="Temps",ylab="Nombre de taches",
+ col=period,type="p",pch="+")
+
+## ----teacher=correct--------------------------------------
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+plot(maxO3~T12,data=ozone)
+hist(ozone$T12)
+boxplot(ozone$maxO3)
 
 ## ---------------------------------------------------------
 library(tidyverse)
@@ -56,6 +93,15 @@ ggplot(diamonds2) #nothing
 ggplot(diamonds2)+aes(x=carat,y=price) #nothing
 ggplot(diamonds2)+aes(x=carat,y=price)+geom_point() #OK
 
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=carat)+geom_histogram()
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=carat)+geom_histogram(bins=10)
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=cut)+geom_bar()
+
 ## ----eval=FALSE-------------------------------------------
 #  ggplot(diamonds2)+aes(x=carat,y=price)
 
@@ -64,6 +110,15 @@ ggplot(diamonds2)+aes(x=carat,y=price)+geom_point() #OK
 
 ## ---------------------------------------------------------
 ggplot(diamonds2)+aes(x=carat,y=price,color=cut)+geom_point()
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=cut)+geom_bar(fill="blue")
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=cut,fill=cut)+geom_bar()
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=cut)+geom_bar(fill=c("blue","red","green","yellow","black"))
 
 ## ---------------------------------------------------------
 D <- data.frame(X=seq(-2*pi,2*pi,by=0.01))
@@ -77,6 +132,18 @@ ggplot(diamonds2)+aes(x=price,y=..density..)+geom_histogram(bins=40)
 
 ## ----eval=FALSE,echo=TRUE---------------------------------
 #  ggplot(diamonds2)+aes(x=price,y=..density..)+stat_bin()
+
+## ----teacher=correct--------------------------------------
+X <- data.frame(X1=c("red","blue","green","black"),prob=c(0.3,0.2,0.4,0.1))
+ggplot(X)+aes(x=X1,y=prob,fill=X1)+geom_bar(stat="identity")+labs(fill="Color")+xlab("")
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_smooth(method="loess")
+ggplot(diamonds2)+aes(x=carat,y=price)+stat_smooth(method="loess")
+
+## ----teacher=correct--------------------------------------
+ggplot(diamonds2)+aes(x=carat,y=price)+geom_smooth(method="loess",linetype="dotted")
+ggplot(diamonds2)+aes(x=carat,y=price)+stat_smooth(method="loess",geom="point")
 
 ## ---------------------------------------------------------
 ggplot(diamonds2)+aes(x=carat,y=price,color=cut)+geom_point()+
@@ -140,9 +207,86 @@ p+theme_classic()
 p+theme_grey()
 p+theme_bw()
 
+## ----teacher=correct--------------------------------------
+X <- seq(-2*pi,2*pi,by=0.001)
+Y1 <- cos(X)
+Y2 <- sin(X)
+donnees1 <- data.frame(X,Y1)
+donnees2 <- data.frame(X,Y2)
+ggplot(donnees1)+geom_line(aes(x=X,y=Y1))+
+  geom_line(data=donnees2,aes(x=X,y=Y2),color="red")
+
+## ----teacher=correct--------------------------------------
+donnees <- data.frame(X,Y1,Y2)
+ggplot(donnees)+aes(x=X,y=Y1)+geom_line()+
+  geom_line(aes(y=Y2),color="red")
+#or for the legend
+ggplot(donnees)+aes(x=X,y=Y1)+geom_line(aes(color="cos"))+
+  geom_line(aes(y=Y2,color="sin"))+labs(color="Fonction")
+
+## ----teacher=correct--------------------------------------
+df <- data.frame(X,cos=Y1,sin=Y2)
+df1 <- df %>% pivot_longer(cols=c(cos,sin),
+                       names_to = "Fonction",
+                       values_to = "value")
+#or
+df1 <- df %>% pivot_longer(cols=-X,
+                       names_to = "Fonction",
+                       values_to = "value")
+ggplot(df1)+aes(x=X,y=value,color=Fonction)+geom_line()
+
+## ----teacher=correct--------------------------------------
+ggplot(df1)+aes(x=X,y=value)+geom_line()+facet_wrap(~Fonction)
+
+## ----teacher=correct--------------------------------------
+library(gridExtra)
+p1 <- ggplot(donnees1)+aes(x=X,y=Y1)+geom_line()
+p2 <- ggplot(donnees2)+aes(x=X,y=Y2)+geom_line()
+grid.arrange(p1,p2,nrow=1)
+
 ## ---------------------------------------------------------
 data(mtcars)
 summary(mtcars)
+
+## ----teacher=correct--------------------------------------
+ggplot(mtcars)+aes(x=mpg)+geom_histogram()
+ggplot(mtcars)+aes(x=mpg)+geom_histogram(bins=10)
+
+## ----teacher=correct--------------------------------------
+ggplot(mtcars)+aes(x=mpg,y=..density..)+geom_histogram(bins=10)
+
+## ----teacher=correct--------------------------------------
+ggplot(mtcars)+aes(x=cyl)+geom_bar()
+
+## ----teacher=correct--------------------------------------
+ggplot(mtcars)+aes(x=disp,y=mpg,color=cyl)+geom_point()
+ggplot(mtcars)+aes(x=disp,y=mpg,color=as.factor(cyl))+geom_point()+labs(color="cyl")
+
+## ----teacher=correct--------------------------------------
+ggplot(mtcars)+aes(x=disp,y=mpg,color=as.factor(cyl))+geom_point()+
+  geom_smooth(method="lm")+labs(color="cyl")
+
+## ----teacher=correct--------------------------------------
+n <- 100
+X <- runif(n)
+eps <- rnorm(n,sd=0.2)
+Y <- 3+X+eps
+D <- data.frame(X,Y)
+
+## ----teacher=correct--------------------------------------
+model <- lm(Y~.,data=D)
+co <- coef(model)
+D$fit <- predict(model)
+co <- coef(lm(Y~.,data=D))
+ggplot(D)+aes(x=X,y=Y)+geom_point()+
+  geom_abline(slope=co[2],intercept=co[1],color="blue")
+
+## ----teacher=correct--------------------------------------
+ggplot(D)+aes(x=X,y=Y)+geom_point()+geom_smooth(method="lm")
+
+## ----teacher=correct--------------------------------------
+ggplot(D)+aes(x=X,y=Y)+geom_point()+geom_smooth(method="lm")+
+  geom_segment(aes(xend=X,yend=fit))
 
 ## ----echo=FALSE,eval=TRUE---------------------------------
 ggplot(data=diamonds) + geom_boxplot(aes(x=cut,y=carat,fill=cut)) 
@@ -181,6 +325,8 @@ ggplot(data=diamonds) +
 
 ## ----echo=FALSE-------------------------------------------
 cache_carto <- FALSE
+correct <- FALSE
+cor <- FALSE
 
 ## ----message=FALSE, warning=FALSE,cache=cache_carto-------
 library(ggmap)
@@ -472,8 +618,8 @@ D %>% plot_ly(x=~X,y=~Y) %>%
          yaxis=list(title="ordonnées"))
 
 ## ----name="plotly_html",eval=!comp_pdf,echo=!comp_pdf-----
-#  plot_ly(z = volcano, type = "surface")
-#  plot_ly(z = volcano, type = "contour")
+plot_ly(z = volcano, type = "surface")
+plot_ly(z = volcano, type = "contour")
 
 ## ----eval=FALSE,echo=FALSE--------------------------------
 #  p <- plot_ly(z = volcano, type = "surface")
@@ -483,7 +629,7 @@ D %>% plot_ly(x=~X,y=~Y) %>%
 #  plot_ly(z = volcano, type = "surface")
 
 ## ----name="plotly_pdf1",eval=comp_pdf,echo=comp_pdf-------
-plot_ly(z = volcano, type = "contour")
+#  plot_ly(z = volcano, type = "contour")
 
 ## ---------------------------------------------------------
 p <- ggplot(iris)+aes(x=Species,y=Sepal.Length)+geom_boxplot()+theme_classic()
@@ -586,10 +732,10 @@ plot(media)
 #                     selected=list("T9"))
 
 ## ----name='app_dash_html',screenshot.opts=list(delay = 5, cliprect = 'viewport',zoom=2,vwidth=200,vheight=200),echo=FALSE,eval=!comp_pdf,out.width=760,out.height=750,indent='        '----
-#  knitr::include_app('https://lrouviere.shinyapps.io/dashboard/', height = '650px')
+knitr::include_app('https://lrouviere.shinyapps.io/dashboard/', height = '650px')
 
 ## ----name='app_dash_pdf',echo=FALSE,eval=comp_pdf,indent='        '----
-webshot::webshot("https://lrouviere.shinyapps.io/dashboard/", file="dashboard.png",delay=20,zoom=1)
+#  webshot::webshot("https://lrouviere.shinyapps.io/dashboard/", file="dashboard.png",delay=20,zoom=1)
 
 ## ---- echo = TRUE, eval = FALSE---------------------------
 #  selectInput(inputId = "color", label = "Couleur :",
@@ -673,14 +819,14 @@ webshot::webshot("https://lrouviere.shinyapps.io/dashboard/", file="dashboard.pn
 #  choices=names(SAheart)[sapply(SAheart,class)=="numeric"]
 
 ## ----name='desc-app_html',screenshot.opts=list(delay = 5, cliprect = 'viewport',zoom=2,vwidth=200,vheight=200),echo=FALSE,eval=!comp_pdf,out.width=760,out.height=750----
-#  knitr::include_app('https://lrouviere.shinyapps.io/DESC_APP/', height = '650px')
+knitr::include_app('https://lrouviere.shinyapps.io/DESC_APP/', height = '650px')
 
 ## ----name='desc-app_pdf',echo=FALSE,eval=comp_pdf---------
-webshot::webshot("https://lrouviere.shinyapps.io/DESC_APP/", file="dashboard.png",delay=5,zoom=1)
+#  webshot::webshot("https://lrouviere.shinyapps.io/DESC_APP/", file="dashboard.png",delay=5,zoom=1)
 
 ## ----name='velib-app_html',screenshot.opts=list(delay = 5, cliprect = 'viewport',zoom=2,vwidth=200,vheight=200),echo=FALSE,eval=!comp_pdf,out.width=760,out.height=750----
-#  knitr::include_app('https://lrouviere.shinyapps.io/velib/', height = '650px')
+knitr::include_app('https://lrouviere.shinyapps.io/velib/', height = '650px')
 
 ## ----name='velib-app_pdf',echo=FALSE,eval=comp_pdf--------
-webshot::webshot("https://lrouviere.shinyapps.io/velib/", file="dashboard.png",delay=5,zoom=1)
+#  webshot::webshot("https://lrouviere.shinyapps.io/velib/", file="dashboard.png",delay=5,zoom=1)
 
